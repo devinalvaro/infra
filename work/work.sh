c4475fbea29d774a8d7a9469c8170a5b3fbda470
Devin Alvaro <devin.alvaro@gmail.com>
2019-11-22 11:38:42 +0800

    Add work.sh

---
+++ work/work.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+export DEBIAN_FRONTEND=noninteractive
+
+UPGRADE_PACKAGES=${1:-none}
+
+echo "==> Running home.sh"
+./home/home.sh ${UPGRADE_PACKAGES}
+
+if [ "${UPGRADE_PACKAGES}" != "none" ]; then
+    echo "==> Updating and upgrading packages"
+    sudo apt-get update
+    sudo apt-get upgrade -y
+fi
+
+echo "==> Installing packages"
+sudo apt-get install -qq \
+    pkg-config \
+    zlib1g-dev \
+    --no-install-recommends
