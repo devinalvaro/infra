0cf3d0661176fa2b3dc980ee25e0f224c76128b4
Devin Alvaro <devin.alvaro@gmail.com>
2019-11-24 12:40:39 +0800

    Enable passing sudo password through parameter

--- home/home.sh
+++ home/home.sh
@@ -4,8 +4,15 @@ set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 
+password="${1-none}"
+
+sudo="sudo"
+if [ "${password}" != "none" ]; then
+    sudo="echo ${password} | sudo -S"
+fi
+
 echo "==> Updating and installing packages"
-sudo apt-get update && apt-get install -qq \
+"${sudo}" apt-get update && "${sudo}" apt-get install -qq \
     build-essential \
     curl \
     docker-compose \
@@ -47,7 +54,7 @@ fi
 
 if [ "$(groups ${USER} | grep -q docker)" ]; then
     echo "==> Adding user to docker group"
-    sudo gpasswd -a "${USER}" docker
+    "${sudo}" gpasswd -a "${USER}" docker
 fi
 
 # fd
@@ -55,7 +62,7 @@ fi
 fd_bin="/usr/local/bin/fd"
 if [ ! -f "${fd_bin}" ]; then
     echo "==> Linking fdfind to fd"
-    sudo ln -sfn "$(which fdfind)" "${fd_bin}"
+    "${sudo}" ln -sfn "$(which fdfind)" "${fd_bin}"
 fi
 
 # fish
@@ -69,7 +76,7 @@ fi
 
 if [ $SHELL != "$(which fish)" ]; then
     echo " ==> Setting fish as the default shell"
-    sudo chsh -s "$(which fish)" "${USER}"
+    "${sudo}" chsh -s "$(which fish)" "${USER}"
 fi
 
 # nvim
--- work/work.sh
+++ work/work.sh
@@ -4,8 +4,15 @@ set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 
+password="${2-none}"
+
+sudo="sudo"
+if [ "${password}" != "none" ]; then
+    sudo="echo ${password} | sudo -S"
+fi
+
 echo "==> Updating and installing packages"
-sudo apt-get update && apt-get install -qq \
+"${sudo}" apt-get update && apt-get install -qq \
     pkg-config \
     zlib1g-dev \
     --no-install-recommends
