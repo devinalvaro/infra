9d681207895ac81e72039b1c257f2e39116e117a
Devin Alvaro <devin.alvaro@gmail.com>
2019-11-24 12:34:39 +0800

    Remove upgrading

--- home/home.sh
+++ home/home.sh
@@ -4,16 +4,8 @@ set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 
-upgrade_packages="${1-none}"
-
-if [ "${upgrade_packages}" == "upgrade" ]; then
-    echo "==> Updating and upgrading packages"
-    sudo apt-get update
-    sudo apt-get upgrade -y
-fi
-
-echo "==> Installing packages"
-sudo apt-get install -qq \
+echo "==> Updating and installing packages"
+sudo apt-get update && apt-get install -qq \
     build-essential \
     curl \
     docker-compose \
