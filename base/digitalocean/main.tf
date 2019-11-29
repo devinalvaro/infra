8ef8b7daf5f9dc7a959501c9911821d622f20bbb
Devin Alvaro <devin.alvaro@gmail.com>
2019-11-29 08:48:52 +0800

    Remove base.sh exec

--- base/digitalocean/main.tf
+++ base/digitalocean/main.tf
@@ -9,30 +9,6 @@ resource "digitalocean_droplet" "base" {
   size      = var.droplet_size
   ssh_keys  = [digitalocean_ssh_key.default.fingerprint]
   user_data = data.template_cloudinit_config.init.rendered
-
-  provisioner "file" {
-    source      = "${path.root}/scripts/base.sh"
-    destination = "/tmp/base.sh"
-
-    connection {
-      user        = var.user
-      host        = digitalocean_droplet.base.ipv4_address
-      private_key = file(var.ssh_key_priv_path)
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/base.sh",
-      "/tmp/base.sh",
-    ]
-
-    connection {
-      user        = var.user
-      host        = digitalocean_droplet.base.ipv4_address
-      private_key = file(var.ssh_key_priv_path)
-    }
-  }
 }
 
 resource "digitalocean_ssh_key" "default" {
--- base/templates/init.tpl
+++ base/templates/init.tpl
@@ -1,10 +1,21 @@
 #cloud-config
+
+groups:
+ - docker
+
 users:
   - name: ${user}
     ssh-authorized-keys:
       - ${ssh_public_key}
     sudo: ['ALL=(ALL) NOPASSWD:ALL']
-    groups: sudo
+    groups:
+      - sudo
+      - docker
     shell: /bin/bash
 
 package_upgrade: true
+
+runcmd:
+  - apt-get install docker.io
+  - systemctl start docker
+  - systemctl enable docker
